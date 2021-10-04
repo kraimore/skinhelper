8043658f787031b5ab5e173e6731ae5b4ec646f0
kraimore <36462756+kraimore@users.noreply.github.com>
2021-10-04 15:14:17 +0200

    Update downsizer.py

--- downsizer.py
+++ downsizer.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import argparse
 import glob
+import os
 input_dir = ""
 
 if __name__ == '__main__':
@@ -9,9 +10,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.input:
         input_dir = args.input
-    for file in glob.glob("*@2x.png"):
+    print(os.path.join(input_dir))
+    input_dir.strip('"')
+    print(os.path.join(input_dir.strip('"')))
+    for file in glob.glob(os.path.join(input_dir.strip('"')) + "\\" + "*@2x.png"):
         img = Image.open(file)
-        (width, height) = (img.width // 2, img.height // 2)
-        img_resized = img.resize((width, height))
-        img_resized.save(file.replace("@2x",""))
-
+        if img.width > 1 and img.height > 1:
+            (width, height) = (img.width // 2, img.height // 2)
+            img_resized = img.resize((width, height))
+            print(input_dir + file.replace("@2x",""))
+            img_resized.save(file.replace("@2x",""))
