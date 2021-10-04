6bf40314c539e1505da671485a174a62951b4e5a
kraimore <36462756+kraimore@users.noreply.github.com>
2021-10-04 16:19:22 +0200

    Update multiplier.py

--- multiplier.py
+++ multiplier.py
@@ -1,27 +1,32 @@
 from PIL import Image
 import argparse
 import os.path
+import glob
 
 
 img_input = ""
 img_output = ""
 n = 0
 f = 0
+hd = ""
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help = "Specify input image (i.e followpoint.png)")
     parser.add_argument("-o", "--output", help = "Specify output image (i.e. followpoint-)")
     parser.add_argument("-n", "--number", help = "Specify image count")
-    parser.add_argument("-f", "--first", help="Specify first image number (default: 0)")
+    parser.add_argument("-f", "--first", help = "Specify first image number (default: 0)")
+    parser.add_argument("-hd", action="store_true", help=  "Specify the elements are HD sized (@2x)")
     args = parser.parse_args()
 
     if args.input:
         img_input = args.input
+    if args.hd:
+        hd = "@2x"
     if args.output:
         img_output = args.output
         i = 0
     else:
-        img_output = args.input.replace(".png", "") + "-"
+        img_output = args.input.replace(".png", "")
         i = 1
     if args.first:
         f = int(args.first)
@@ -30,7 +35,11 @@ if __name__ == '__main__':
     img = Image.open(img_input)
     while n >= f:
         if i == 1:
-            img.save(img_output + str(n) + ".png")
+            img.save(img_output.strip("@2x") + "-" + str(n) + hd + ".png")
         else:
-            img.save(os.path.dirname(img_input)  + "\\" + img_output + str(n) + ".png")
+            if ":\\" in img_input:
+
+                img.save(os.path.dirname(img_input) + "\\" + img_output + "-" + str(n) + hd + ".png")
+            else:
+                img.save(os.path.dirname(img_input) + img_output + "-" + str(n) + hd + ".png")
         n = n - 1
