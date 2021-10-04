03a29fdb45f75305df744a3ccfb3e82dc43a3cec
kraimore <36462756+kraimore@users.noreply.github.com>
2021-10-04 12:16:46 +0200

    Update multiplier.py

--- multiplier.py
+++ multiplier.py
@@ -5,21 +5,25 @@ import argparse
 img_input = ""
 img_output = ""
 n = 0
+f = 0
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help = "Specify input image (i.e followpoint.png)")
     parser.add_argument("-o", "--output", help = "Specify output image (i.e. followpoint-)")
     parser.add_argument("-n", "--number", help = "Specify image count")
+    parser.add_argument("-f", "--first", help="Specify image count")
     args = parser.parse_args()
 
     if args.input:
         img_input = args.input
     if args.output:
         img_output = args.output
+    if args.first:
+        f = int(args.first)
     if args.number:
-        n = int(args.number)
+        n  = int(args.number) + f - 1
     img = Image.open(img_input)
-    while n >= 0:
+    while n >= f:
         img.save(img_output + str(n) + ".png")
         n = n - 1
 
